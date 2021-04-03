f7f9cd2b96240a8efd3542b6f35bd6fe66868c07
Rafael Pinto <rafael12wq@gmail.com>
2021-04-03 13:44:29 -0300

    Make the columns userable_id and userable_table in user migration file

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('userable_id');
+            $table->string('userable_type');
             $table->rememberToken();
             $table->timestamps();
         });
