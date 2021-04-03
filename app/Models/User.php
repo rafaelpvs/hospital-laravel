51640ef7f7add7371683cc632049ea2cde43654c
Rafael Pinto <rafael12wq@gmail.com>
2021-04-03 13:28:47 -0300

    Create relation with nurse in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,6 +16,9 @@ class User extends Authenticatable
      *
      * @var array
      */
+
+
+
     protected $fillable = [
         'name',
         'email',
@@ -40,4 +43,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function userable() {
+        return $this->morphTo();
+    }
+
 }
