4eb2738ce92c81b6a24988dbe25d3422b1ffcd8e
Rafael Pinto <rafael12wq@gmail.com>
2021-04-03 21:09:53 -0300

    Create Phone table migration

---
+++ database/migrations/2021_04_03_235742_create_phones_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePhonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('phones', function (Blueprint $table) {
+            $table->id();
+            $table->integer('patient_id')->index();
+            $table->string('ddd');
+            $table->string('ddi');
+            $table->string('number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('phones');
+    }
+}
