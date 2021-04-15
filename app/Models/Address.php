018a10d458d1f2c59f739109ac3f37af35c8b6b7
Rafael Pinto <rafael12wq@gmail.com>
2021-04-15 19:25:24 -0300

    Add fillable attributes to Address model

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -8,7 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Address extends Model
 {
     use HasFactory;
-
+    protected $fillable = [
+        'name',
+        'cpf',
+        'rg',
+        'birthdate',
+    ];
 
     public function patient() {
         return $this->belongsTo(Patient::class);
