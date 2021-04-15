4f46564830fc69d43b58f12689938ef417975ba2
Rafael Pinto <rafael12wq@gmail.com>
2021-04-15 19:25:44 -0300

    Add fillable attributes to internment model

--- app/Models/Internment.php
+++ app/Models/Internment.php
@@ -8,6 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Internment extends Model
 {
     use HasFactory;
+    protected $fillable  = [
+        'disease',
+    ];
 
     public function apartments() {
         return $this->belongsToMany(Apartment::class);
