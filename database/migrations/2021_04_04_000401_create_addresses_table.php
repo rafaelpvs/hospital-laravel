aa159e70d83c2535e0975ef2e8b63a647522edc4
Rafael Pinto <rafael12wq@gmail.com>
2021-04-15 19:24:43 -0300

    Change the name of address table

--- database/migrations/2021_04_04_000401_create_addresses_table.php
+++ database/migrations/2021_04_04_000401_create_addresses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAddresssTable extends Migration
+class CreateAddressesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateAddresssTable extends Migration
      */
     public function up()
     {
-        Schema::create('addresss', function (Blueprint $table) {
+        Schema::create('addresses', function (Blueprint $table) {
             $table->id();
             $table->integer('patient_id');
             $table->string('street');
@@ -32,6 +32,6 @@ class CreateAddresssTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('addresss');
+        Schema::dropIfExists('addresses');
     }
 }
