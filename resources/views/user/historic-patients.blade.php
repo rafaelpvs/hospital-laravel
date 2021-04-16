24fe907b5f7a27597ccb59c0ee39bb0cbc630d9c
Rafael Pinto <rafael12wq@gmail.com>
2021-04-16 16:21:48 -0300

    Create historic view

---
+++ resources/views/user/historic-patients.blade.php
@@ -0,0 +1,41 @@
+<x-master>
+    @section('content')
+        <table class="table">
+            <thead class="thead-dark">
+            <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Age</th>
+                <th scope="col">Disease</th>
+                <th scope="col">Interned</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($internments as $internment)
+                    <tr>
+                        <th><a href="{{ route('patients.show',$internment->patient->id) }}">{{$internment->patient->name}}</a></th>
+                        <td>{{$internment->patient->age()}}</td>
+                        <td>{{$internment->disease}}</td>
+                        <td>
+
+                            @if($internment->departure_time == null)
+
+                                <form method="POST" action="{{ route('internments.update', $internment->id) }}">
+                                    @csrf
+                                    <input name="_method" type="hidden" value="PATCH">
+                                    <input type="submit" class="btn btn-success" value="Discharge">
+                                </form>
+                            @else
+                                <form method="POST" action="{{ route('internments.update', $internment->id) }}">
+                                    @csrf
+                                    <input name="_method" type="hidden" value="PATCH">
+                                    <input type="submit" class="btn btn-danger" value="Intern">
+                                </form>
+                            @endif
+
+                        </td>
+                    </tr>
+            @endforeach
+            </tbody>
+        </table>
+    @endsection
+</x-master>
