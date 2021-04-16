0cf1ec8f193f45173012db4ed092cad7afb77128
Rafael Pinto <rafael12wq@gmail.com>
2021-04-16 16:18:44 -0300

    Add more features to patient show view

--- resources/views/user/show-patient.blade.php
+++ resources/views/user/show-patient.blade.php
@@ -38,6 +38,32 @@
                     {{$patient->rg}}
                 </div>
             </div>
+            <hr>
+            <div class="row">
+                <div class="col-sm-3">
+                    <h6 class="mb-0">Current interned</h6>
+                </div>
+                <div class="col-sm-9 text-secondary">
+{{--                    {{dd($patient->isInterned())}}--}}
+                    @if($patient->isInterned() == true)
+
+                        <form method="POST" action="{{ route('internments.update', $patient->currentInternment()->id) }}">
+                            @csrf
+                            <input name="_method" type="hidden" value="PATCH">
+
+                            <input type="submit" class="btn btn-success" value="Discharge">
+                        </form>
+                    @else
+                        <form method="POST" action="{{ route('internments.store') }}">
+                            @csrf
+                            <input name="_method" type="hidden" value="POST">
+                            <input name="patientid" type="hidden" value="{{$patient->id}}">
+                            <input name="disease" class="form-control mb-3" type="text">
+                            <input type="submit" class="btn btn-danger" value="New disease">
+                        </form>
+                    @endif
+                </div>
+            </div>
 
         </div>
     </div>
@@ -63,28 +89,37 @@
             <hr>
             <div class="row">
                 <div class="col-sm-3">
-                    <h6 class="mb-0">Idade</h6>
+                    <h6 class="mb-0">District</h6>
                 </div>
                 <div class="col-sm-9 text-secondary">
-                    {{ $patient->age() }}
+                    {{ $patient->address->district }}
                 </div>
             </div>
             <hr>
             <div class="row">
                 <div class="col-sm-3">
-                    <h6 class="mb-0">Cpf</h6>
+                    <h6 class="mb-0">City</h6>
                 </div>
                 <div class="col-sm-9 text-secondary">
-                    {{$patient->cpf}}
+                    {{ $patient->address->city }}
                 </div>
             </div>
             <hr>
             <div class="row">
                 <div class="col-sm-3">
-                    <h6 class="mb-0">Rg</h6>
+                    <h6 class="mb-0">Zipcode</h6>
                 </div>
                 <div class="col-sm-9 text-secondary">
-                    {{$patient->rg}}
+                    {{ $patient->address->zipcode }}
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-sm-3">
+                    <h6 class="mb-0">State</h6>
+                </div>
+                <div class="col-sm-9 text-secondary">
+                    {{ $patient->address->state }}
                 </div>
             </div>
 
@@ -92,14 +127,36 @@
     </div>
 
 
+
+    <div class="row">
+        <div class="col-sm-12 text-center">
+            <input class="btn btn-primary btn-md" value="Edit">
+        </div>
     </div>
-        <div class="row">
-            <div class="col-sm-12 text-center">
-                <button class="btn btn-primary btn-md">Edit</button>
-                <button class="btn btn-danger btn-md">Delete</button>
+
+        <div class="card mb-3">
+
+
+
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-sm-3">
+                        <h5 class="mb-0">Phones</h5>
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-sm-3">
+                        <h6 class="mb-0 bold">Street</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                        {{ $patient->address->street }}
+                    </div>
+                </div>
+
+
             </div>
         </div>
-
 
     @endsection
 
